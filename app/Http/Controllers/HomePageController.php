a68558371faa2bbe0466445af87834ed74448c57
fercamm84 <fernando.cammarota@gmail.com>
2018-03-21 01:00:49 -0300

    Recorrido para obtener productos por categoria.

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Section;
+
 class HomePageController extends Controller
 {
 
@@ -9,8 +11,19 @@ class HomePageController extends Controller
     }
 
     public function index(){
+        //Traer listado de "section" que tenga "section_categories" y "section_products" y esos mismos tengan "section"
+        // y "category" y "section" y "product" respectivamente.
+        $sections = Section::all();
+        foreach($sections as $section){
+            foreach($section->sectionCategories as $sectionCategory){
+//                print_r($sectionCategory->section->name);
+//                print_r($sectionCategory->category->description);
+                foreach($sectionCategory->category->categoryProducts as $productCategory){
+                    print_r($productCategory->product->name);
+                }
+            }
+        }
 
-
-        return view('home.home');
+        return view('home.home', array('sections' => $sections));
     }
 }
