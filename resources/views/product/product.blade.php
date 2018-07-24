2279d7971fa69d30ccc88f2d2980676867f2ac23
fercamm84 <fernando.cammarota@gmail.com>
2018-07-23 23:59:20 -0300

    Envio de email desde la pagina de contactanos.

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -72,6 +72,7 @@
                 <h4>Show it to your friends</h4>
                 <p>
                     <a href="#" class="external facebook" data-animate-hover="pulse"><i class="fa fa-facebook"></i></a>
+                    <a class="post_share_facebook external facebook" data-animate-hover="pulse" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.marcoinversiones.com/marco-inversiones-huergo.html"><i class="fa fa-facebook"></i></a>
                     <a href="#" class="external gplus" data-animate-hover="pulse"><i class="fa fa-google-plus"></i></a>
                     <a href="#" class="external twitter" data-animate-hover="pulse"><i class="fa fa-twitter"></i></a>
                     <a href="#" class="email" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
