b87eedc9f77672a5cead44be22754b81fa79f4ea
fercamm84 <fernando.cammarota@gmail.com>
2018-09-26 10:22:35 -0300

    Validacion si no tiene dias de reservas.

--- resources/views/seller/reservation.blade.php
+++ resources/views/seller/reservation.blade.php
@@ -3,38 +3,38 @@
 @section('details')
 
     @if($sellerDay)
-    @foreach($hours as $hour => $availability)
-        <div class="hour">
-            {{ $hour }} -
-            @if($availability >= $number_of_reservations)
-                {{ Form::open(array('id' => 'formulario', 'action' => 'SellerShowController@reserve')) }}
-                <p class="text-center buttons">
-                    {{ Form::hidden('seller_day_id', $seller->id) }}
-                    {{ Form::hidden('number_of_reservations', $number_of_reservations) }}
-                    {{ Form::hidden('from_hour', $hour) }}
+        @foreach($hours as $hour => $availability)
+            <div class="hour">
+                {{ $hour }} -
+                @if($availability >= $number_of_reservations)
+                    {{ Form::open(array('id' => 'formulario', 'action' => 'SellerShowController@reserve')) }}
+                    <p class="text-center buttons">
+                        {{ Form::hidden('seller_day_id', $seller->id) }}
+                        {{ Form::hidden('number_of_reservations', $number_of_reservations) }}
+                        {{ Form::hidden('from_hour', $hour) }}
 
-                    <?php $maxHours = 1; ?>
-                    @foreach($hours as $nextHour => $availabilityNextHour)
-                        @if($nextHour > $hour)
-                            @if($availabilityNextHour >= $number_of_reservations)
-                                <?php $maxHours++; ?>
-                            @else
-                                @break
+                        <?php $maxHours = 1; ?>
+                        @foreach($hours as $nextHour => $availabilityNextHour)
+                            @if($nextHour > $hour)
+                                @if($availabilityNextHour >= $number_of_reservations)
+                                    <?php $maxHours++; ?>
+                                @else
+                                    @break
+                                @endif
                             @endif
-                        @endif
-                    @endforeach
+                        @endforeach
 
-                    Cant. horas: {{ Form::number('hours', null, ['class' => 'form-control hour', 'min' => '1', 'max' => $maxHours, 'placeholder' => 'Max: '.$maxHours.' hs.', 'type' => 'number', 'required' => true]) }}
-                    <button class='btn btn-primary' type='submit' value='submit' style="margin-top:3%;">
-                        <i class='fa fa-user'></i> Reservar
-                    </button>
-                </p>
-                {{ Form::close() }}
-            @else
-                NO HAY LUGAR
-            @endif
-        </div>
-    @endforeach
+                        Cant. horas: {{ Form::number('hours', null, ['class' => 'form-control hour', 'min' => '1', 'max' => $maxHours, 'placeholder' => 'Max: '.$maxHours.' hs.', 'type' => 'number', 'required' => true]) }}
+                        <button class='btn btn-primary' type='submit' value='submit' style="margin-top:3%;">
+                            <i class='fa fa-user'></i> Reservar
+                        </button>
+                    </p>
+                    {{ Form::close() }}
+                @else
+                    NO HAY LUGAR
+                @endif
+            </div>
+        @endforeach
     @else
         NO SE HACEN RESERVAS PARA ESTE DIA
     @endif
