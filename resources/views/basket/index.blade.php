5f7c3a5d6b29c402c12acf86e41f4c7d72560377
fercamm84 <fernando.cammarota@gmail.com>
2018-12-13 01:16:21 -0300

    https

--- resources/views/basket/index.blade.php
+++ resources/views/basket/index.blade.php
@@ -75,7 +75,7 @@
                 var s = document.createElement("script");
                 s.type = "text/javascript";
                 s.async = true;
-                s.src = ("https:" == document.location.protocol ? "https://www.mercadopago.com/org-img/jsapi/mptools/buttons/" : "http://mp-tools.mlstatic.com/buttons/") + "render.js";
+                s.src = ("https:" == document.location.protocol ? "https://www.mercadopago.com/org-img/jsapi/mptools/buttons/" : "https://mp-tools.mlstatic.com/buttons/") + "render.js";
                 var x = document.getElementsByTagName('script')[0];
                 x.parentNode.insertBefore(s, x);
                 window.$MPBR_loaded = true;
